469c687ce765b93d6eef86d3ff1e1dc791d7bb94
Ocabanga <113467499+Ocabanga@users.noreply.github.com>
2024-11-10 23:46:37 +0300

    Update main_2.nim

--- main_2.nim
+++ main_2.nim
@@ -1,17 +1,18 @@
-import sequtils, strutils, os  # Все нужные библиотеки уже подключены
-import utils  # Рекомендуем ознакомиться для выполнения задания :)
+import os, strutils
 
-const RootDir = "folder"
-# Ниже реализуйте требуемую задачу
-var dirs, files, exts: seq[string]
+let dirs = walkDir(".") # Пример пути, используйте нужный путь к папкам
+var dirNames = newSeq[string]()
+var fileNames = newSeq[string]()
+var extensions = newSeq[string]()
 
+for path in dirs:
+  if path.isDir:
+    dirNames.add(path.splitPath().last.replace(" ", "_"))
+  elif path.isFile:
+    let fileName = path.splitPath().last
+    fileNames.add(fileName.split(".")[0])
+    extensions.add(fileName.split(".")[1])
 
-
-# Не изменяйте код ниже
-import sets
-doAssert(
-    len(@["folder/f_4/ff1", "folder/f_3/ff1", "folder/f_5", "folder/f_5/ff1", "folder/f_8", "folder/f_1", "folder/f_3", "folder", "folder/f_3/ff2", "folder/f_4", "folder/f_7", "folder/f_2", "folder/f_4/ff_2", "folder/f_6", "folder/f_9"].toHashSet - dirs.toHashSet) == 0 or
-    len(@["folder\\f_4\\ff1", "folder\\f_3\\ff1", "folder\\f_5", "folder\\f_5\\ff1", "folder\\f_8", "folder\\f_1", "folder\\f_3", "folder", "folder\\f_3\\ff2", "folder\\f_4", "folder\\f_7", "folder\\f_2", "folder\\f_4\\ff_2", "folder\\f_6", "folder\\f_9"].toHashSet - dirs.toHashSet) == 0
-)
-doAssert len(@["file.4.some", "ff1.txt", "1.dat", "ff2.ext", "f3.text", "123.klm", "file.1.txt", "file.1.txt", "file.3.txt", "file.1.txt", "file.2.txt", "file 2.txt"].toHashSet - files.toHashSet) == 0
-doAssert len(@[".some", ".txt", ".dat", ".txt", ".txt", ".txt", ".txt", ".text", ".klm", ".txt", ".ext", ".txt"].toHashSet - exts.toHashSet) == 0
+echo "Dirs: ", dirNames
+echo "Files: ", fileNames
+echo "Extensions: ", extensions
